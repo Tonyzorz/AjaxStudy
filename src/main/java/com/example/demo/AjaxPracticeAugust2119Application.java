ba2c9ec3f5ca2587d51f37abe7299b179e21836f
taewon <tonyzorz@naver.com>
2019-08-21 01:07:39 +0900

    start

---
+++ src/main/java/com/example/demo/AjaxPracticeAugust2119Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AjaxPracticeAugust2119Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AjaxPracticeAugust2119Application.class, args);
+	}
+
+}
